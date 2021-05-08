ae810a59dc31b9d7419cf2a922013678d2ae6ac4
omkar2005203 <raneomkar32@gmail.com>
2021-05-08 13:26:25 +0530

    exception handling

--- ExceptionHandling.php
+++ ExceptionHandling.php
@@ -13,11 +13,30 @@ show_source($fileName); // to show source code
 <?php
 
 echo "<h3 class='text-primary' >Output:</h3>";
-Class NewClass{
 
-    
+    $num=8;
 
-}
+    try{
+        if($num>=10){
+            echo "</br>";
+            echo "$num is greater than 10";
+        }
+            
+        else{
+            throw new Exception("Enter Value greater than equal to 10");
+        }
+
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+    finally{
+        echo "</br>";
+        echo "Runs everytime !";
+       
+    }
+
+
 ?>
 
 
